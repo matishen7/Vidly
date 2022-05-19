a3bdba9dcb56b6d11724ecc79cd539af6f2e81be
Muratbek Ishenbaev <MIshenba@win.cccis.com>
2022-05-19 12:41:21 -0700

    Seeding membershipType table

---
+++ Migrations/202205191916219_AddSubscribedToNewsToCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddSubscribedToNewsToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscribedToNews", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscribedToNews");
+        }
+    }
+}
