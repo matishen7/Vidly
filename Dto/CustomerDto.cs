1679c73e360bf887412840e01d729e0beecaffab
Muratbek Ishenbaev <MIshenba@win.cccis.com>
2022-06-05 15:40:48 -0700

    customerDto

---
+++ Dto/CustomerDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dto
+{
+    public class CustomerDto
+    {
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscribedToNews { get; set; }
+
+        public int MembershipTypeId { get; set; }
+
+        public DateTime? Birthdate { get; set; }
+    }
+}
