2fd40ad9f302a7221ad8ab11b5a7f8f53fa917fe
Muratbek Ishenbaev <MIshenba@win.cccis.com>
2022-06-09 12:23:43 -0700

    moviescontroller api

---
+++ Controllers/Api/MoviesController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using Vidly.Dto;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetMovies()
+        {
+            var result = _context.Movies.Include(c => c.Genre).ToList().Select(Mapper.Map<Movie, MovieDto>);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetMovies(int id)
+        {
+            var movie = _context.Movies.Include(c => c.Genre).SingleOrDefault(x => x.Id == id);
+
+            if (movie == null)
+                return NotFound();
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movie = Mapper.Map<MovieDto, Movie>(movieDto);
+
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == movieDto.GenreId);
+            if (genre.Name == null)
+                return NotFound();
+
+            movie.Genre = genre;
+
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+            movieDto.Id = movie.Id;
+
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var movieInDb = _context.Movies.Include(c => c.Genre).SingleOrDefault(x => x.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            var genre = _context.Genres.SingleOrDefault(x => x.Id == movieDto.GenreId);
+            if (genre.Name == null)
+                return NotFound();
+            Mapper.Map(movieDto, movieInDb);
+            //customerInDb.Name = customerDto.Name;
+            //customerInDb.Birthdate = customerDto.Birthdate;
+            //customerInDb.IsSubscribedToNews = customerDto.IsSubscribedToNews;
+            //customerInDb.MembershipType = membershipType;
+
+            _context.SaveChanges();
+            return Ok(movieDto);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var movieInDb = _context.Movies.Include(c => c.Genre).SingleOrDefault(x => x.Id == id);
+
+            if (movieInDb == null)
+                return NotFound();
+
+            _context.Movies.Remove(movieInDb);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ Dto/MovieDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Dto
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        [Range(1, 20)]
+        public int NumberInStock { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+    }
+}
